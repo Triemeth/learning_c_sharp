f6201c154dd4b2ede3dc3a23690c0a1cfe5e2c54
Ethan Triem <EJTriem@outlook.com>
2025-09-22 17:36:55 -0500

    firguring this out

---
+++ Basics/output.cs
@@ -0,0 +1,16 @@
+// using System;
+
+// namespace outputs{
+//   class Program{
+//     static void Main(string[] args){
+//         //normal
+//         Console.WriteLine("Hello");
+//         //math calcs
+//         Console.WriteLine(3+3);
+//         //write is similar to writeline but no newline 
+//         Console.Write("Hello ");
+//         Console.Write("World");
+
+//     }
+//   }
+// }
