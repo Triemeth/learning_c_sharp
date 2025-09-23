a9949a16132c1b3576bfdac80bd096ad700ce9d9
Ethan Triem <EJTriem@outlook.com>
2025-09-22 21:22:16 -0500

    vars

---
+++ Basics/variables.cs
@@ -0,0 +1,23 @@
+// using System;
+
+// namespace vars{
+//     class Program{
+//         static void Main(string[] args){
+//             int mynum = 5;
+//             double mydub = 5.9;
+//             char myletter = 'E';
+//             bool mybool = true;
+//             string mytext = "Text";
+
+//             Console.WriteLine(mytext);
+
+//             int unassigned_num;
+//             unassigned_num = 15;
+
+//             mydub = 20.26;
+//             Console.Write(mydub);
+
+
+//         }
+//     }
+// }
