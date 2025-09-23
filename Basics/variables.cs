4332f4ad155695077f5c480fef6cfca3d7f958c3
Ethan Triem <EJTriem@outlook.com>
2025-09-22 21:36:45 -0500

    comment out main

--- Basics/variables.cs
+++ Basics/variables.cs
@@ -1,43 +1,43 @@
-using System;
+// using System;
 
-namespace vars{
-    class Program{
-        static void Main(string[] args){
-            // Variables Basics
-            int mynum = 5;
-            double mydub = 5.9;
-            char myletter = 'E';
-            bool mybool = true;
-            string mytext = "Text";
+// namespace vars{
+//     class Program{
+//         static void Main(string[] args){
+//             // Variables Basics
+//             int mynum = 5;
+//             double mydub = 5.9;
+//             char myletter = 'E';
+//             bool mybool = true;
+//             string mytext = "Text";
 
-            Console.WriteLine(mytext);
+//             Console.WriteLine(mytext);
 
-            int unassigned_num;
-            unassigned_num = 15;
+//             int unassigned_num;
+//             unassigned_num = 15;
 
-            mydub = 20.26;
-            Console.WriteLine(mydub);
+//             mydub = 20.26;
+//             Console.WriteLine(mydub);
 
-            // Constants
-            const int constVal = 25;
+//             // Constants
+//             const int constVal = 25;
 
-            // Display Variables
-            string name = "John";
-            Console.WriteLine("Hell0 " + name);
+//             // Display Variables
+//             string name = "John";
+//             Console.WriteLine("Hell0 " + name);
 
-            string fname = "John ";
-            string lname = "Doe";
-            string full_name = fname + lname;
-            Console.WriteLine(full_name);
+//             string fname = "John ";
+//             string lname = "Doe";
+//             string full_name = fname + lname;
+//             Console.WriteLine(full_name);
 
-            // Multiple Variables
-            int x = 5, y = 6, z = 50;
-            Console.WriteLine(x + y + z);
+//             // Multiple Variables
+//             int x = 5, y = 6, z = 50;
+//             Console.WriteLine(x + y + z);
 
-            int a, b, c;
-            a = b = c = 50;
-            Console.WriteLine(a + b + c);
+//             int a, b, c;
+//             a = b = c = 50;
+//             Console.WriteLine(a + b + c);
 
-        }
-    }
-}
+//         }
+//     }
+// }
